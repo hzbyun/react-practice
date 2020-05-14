5f3bf33240fa14b52f133de24a27015c4777c683
James Byun <james.byun@myob.com>
2020-05-15 09:13:43 +1000

    why we need jsx

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, Hello</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, Hello</h1>
+    //   </div>
+    // );
+
+    // ^
+    // |
+    // |
+    // the code below is same as above. Cumbersome to write.. This is why 
+    // jsx (above) is really needed and helps us.
+    return React.createElement(
+      'div', 
+      {className: 'App'}, 
+    React.createElement('h1', null, 'Hi, Hello !!'));
   }
 }
 
