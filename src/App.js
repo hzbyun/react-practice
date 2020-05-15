3cdfe1d5c56424a2ef312b0559a45ad960c8598a
James Byun <james.byun@myob.com>
2020-05-15 18:22:43 +1000

    handle events and how to trigger with methods

--- src/App.js
+++ src/App.js
@@ -11,12 +11,20 @@ class App extends Component {
       { name: 'Isabella', age: 31}
     ]
   }
+
+  switchNameHandler = () => {
+    console.log('clicked');
+  }
   
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button>Change Name</button>
+        {/* DO NOT add () at the end of this.switchNameHandler
+            this.switchNameHandler() means it will excute this function whenever render components
+            Just assign the reference by removing () at the end
+        */}
+        <button onClick={this.switchNameHandler}>Change Name</button>
         <Person name={this.state.person[0].name} age={this.state.person[0].age}> props.children will render this </Person>
         <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
       </div>
