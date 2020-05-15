5afc3157c840b411b19c80eedad70a0c30b60afa
James Byun <james.byun@myob.com>
2020-05-15 17:49:53 +1000

    Outputting a Dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const person = () => {
-    return <p>I'm a Person!</p>
+    // This is how to add js code in the middle of jsx
+    return <p>I'm a Person! I am {Math.floor(Math.random() * 30)} years old.</p>
 }
 
 export default person;
