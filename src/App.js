78ff7087f2abe98e07137f887ed9a6bde2fa23ae
James Byun <james.byun@myob.com>
2020-05-15 18:14:45 +1000

    what is state, how to access it within component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // this state is the only property that we can use under the class inherited by Component
+  // *if this state is changed, react render this component. (run render() again)
+  state = {
+    person: [
+      { name: 'James', age: 32},
+      { name: 'Isabella', age: 31}
+    ]
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="James" age="32"> props.children will render this </Person>
-        <Person name="Isabella" age="31"/>
+        <button>Change Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}> props.children will render this </Person>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
       </div>
     );
 
