8aa3875acc7c4d5a761f5e9962dce733d986288f
James Byun <james.byun@myob.com>
2020-05-18 08:26:17 +1000

    revert into class component again

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  // useState takes 2 elements
-  // 1. Current state
-  // 2. function to update the states
-
-  // const [..] <- It is called Destructuring assignment
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  // this state is the only property that we can use under the class inherited by Component
+  // *if this state is changed, react render this component. (run render() again)
+  state = {
     persons: [
       { name: 'James', age: 32},
       { name: 'Isabella', age: 31}
     ]
-  });
+  }
 
-  const switchNameHandler = () => {
-    // Important : setPersonsState does NOT merge a state, BUT replace a state.
-    // That is why seggregating useState for each business case is a common practice.
-    setPersonsState({
+  switchNameHandler = () => {
+    // Don't do this!! react does not recommend to mutate the state directly
+    //this.state.persons[0].name = "Issachar"
+    this.setState({
         persons: [
         { name: 'James', age: 31},
         { name: 'Isabella', age: 30}
       ]
     })
   }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        {/* DO NOT add () at the end of this.switchNameHandler
+            this.switchNameHandler() means it will excute this function whenever render components
+            Just assign the reference by removing () at the end
+        */}
+        <button onClick={this.switchNameHandler}>Change Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> props.children will render this </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      </div>
+    );
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      {/* DO NOT add () at the end of this.switchNameHandler
-          this.switchNameHandler() means it will excute this function whenever render components
-          Just assign the reference by removing () at the end
-      */}
-      <button onClick={switchNameHandler}>Change Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}> props.children will render this </Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-    </div>
-  );
+    // return React.createElement(
+    //   'div', 
+    //   {className: 'App'}, 
+    // React.createElement('h1', null, 'Hi, Hello !!'));
+  }
 }
 
-export default app;
+export default App;
